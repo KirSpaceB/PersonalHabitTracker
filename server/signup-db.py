016026119f5f6103b6236fa1526d69a64f2dd8eb
KirSpaceB <productivekirk@gmail.com>
2023-03-03 13:10:06 -0800

    fix: fixed the login feature to interact with backend only

--- server/signup-db.py
+++ server/signup-db.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request,jsonify,redirect, url_for,render_template
 from flask_cors import CORS
 import mysql.connector
 
@@ -36,7 +36,7 @@ def signup_db():
 
     return {"message": "Successfully Registered"}
 
-@app.route('/user-auth', methods=["GET"])
+@app.route('/user-auth', methods=["POST"])
 def authenticate_user():
 
     DB = mysql.connector.connect(
@@ -45,21 +45,33 @@ def authenticate_user():
         host="localhost",
         database="habit_tracker_users" #Species what tb to use when creating tables etc.
     )
-    
+    loginInfo = request.get_json()
+    print(loginInfo)
+
+
     sqlCursor = DB.cursor()
-
-    query = ("SELECT * FROM users")
-
-    sqlCursor.execute(query)
-
+    #SQL command to execute
+    checkForUser = ("SELECT * FROM users")
+    #Execute query
+    sqlCursor.execute(checkForUser)
+    #Fetches all the selected users in the DB
     results = sqlCursor.fetchall()
 
-    print(results)
-    for result in results:
-        print(result)
-    return results
-    
+    for row in results:
+        print('testicles')
+        if row[0] == loginInfo['userName'] and row[1] == loginInfo['password']:
+            print('user is in')
+            return jsonify({'message':'success'})
+        else:
+            print('login failed')
+            return jsonify({'message':'failed'})
+        #This conditional works but, I want it to redirect the user to a different react component changing the ui 
 
+    return
+    
+@app.route('/HabitTracker/Home')
+def redirectToReactComponent():
+    return render_template('HabitTracker/Home.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
