5561cf2f5762543ba390060aadcdfa92caffccf5
KirSpaceB <productivekirk@gmail.com>
2023-03-30 12:54:05 -0700

    fix: 'jest was missing css module transpilers'

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,13 +1,15 @@
-export const roots = ["<rootDir>/src"];
 export default {
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect'
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.ts', // Use .ts instead of .js
+  }
 };
-export const transform = {
-  "^.+\\.tsx?$": "ts-jest"
-};
-export const setupFilesAfterEnv = [
-  "@testing-library/react/cleanup-after-each",
-  "@testing-library/jest-dom/extend-expect"
-];
-export const testRegex = "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$";
-export const moduleFileExtensions = ["ts", "tsx", "js", "jsx", "json", "node"];
--- client/src/components/tests/LoginPage.test.tsx
+++ client/src/components/tests/LoginPage.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import {render,fireEvent,waitFor} from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import LoginPage from '../LoginPage'
+import { postUserLogin } from '../../services/user-auth'
+import styles from '../styles/LoginPage.module.css';
 
-describe('<LoginPage/>', () => {
-  test('Display the loginpage component', async () => {
-    const checkToRender = render(<LoginPage/>)
-    expect(checkToRender)
+
+jest.mock('../../services/user-auth', () => ({
+  postUserLogin: jest.fn(),
+}))
+
+describe('LoginPage', () => {
+  it('Renders login page', async () => {
+    render(<LoginPage/>)
   })
 })
